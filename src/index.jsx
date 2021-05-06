4de78e90a9e1769543142132d9a06dba36aebd5f
therandomfool <mddenver@protonmail.com>
2021-05-05 18:01:36 -0700

    continue

--- src/index.jsx
+++ src/index.jsx
@@ -1,40 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
+// img: 'https://m.media-amazon.com/images/I/71+lh7jD7qL._AC_UL320_.jpg',
+
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/91wtqxlMHdL._AC_UL320_.jpg',
+  title: 'The Overstreet Comic Book Price',
+  author: 'Robert M. Overstreet'
+}
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/71+lh7jD7qL._AC_UL320_.jpg',
+  title: 'Cloudbuster Nine',
+  author: 'Anne R. Keene'
+}
 
 
-function BookList(){
-  return(
+function BookList() {
+  return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      
+      <Book
+        img={firstBook.img} 
+        title={firstBook.title} 
+        author={firstBook.author}>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam ea, ipsam veniam iure odit molestias illo quasi in architecto nesciunt?</p>
+        </Book>
+      <Book
+        img={secondBook.img} 
+        title={secondBook.title} 
+        author={secondBook.author} />
+
     </section>
   )
 }
 
-const Book = () => {
+const Book = ({img, title, author, children}) => {
+  // const {img, title, author} = props
   return <article>
-    <Image />
-    <Title />
-    <Author />
+    <img src={img} alt='' />
+    <h1>{title}</h1>
+    <h4>{author}</h4>
+    {children}
   </article>
 }
-
-const Image = () => <img src='https://m.media-amazon.com/images/I/91wtqxlMHdL._AC_UL320_.jpg' alt=''/>
-
-const Title = () => <h1>The Overstreet Comic Book Price</h1>
-
-const Author =  () => <h4>Robert M. Overstreet</h4>
 
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
