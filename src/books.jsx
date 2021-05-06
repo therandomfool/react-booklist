867a070e9d4fc868e2ae4e4c2cdc5d759b4cbfcf
therandomfool <mddenver@protonmail.com>
2021-05-05 19:11:14 -0700

    refactor phase 1

---
+++ src/books.jsx
@@ -0,0 +1,19 @@
+
+
+export const data = [
+    { id:1,
+      img: 'https://m.media-amazon.com/images/I/91wtqxlMHdL._AC_UL320_.jpg',
+      title: 'The Overstreet Comic Book Price',
+      author: 'Robert M. Overstreet'
+    },
+    { id:2,
+      img: 'https://m.media-amazon.com/images/I/71+lh7jD7qL._AC_UL320_.jpg',
+      title: 'Cloudbuster Nine',
+      author: 'Anne R. Keene'
+    },
+    { id:3,
+      img: 'https://m.media-amazon.com/images/I/91lslnZ-btL._AC_UL320_.jpg',
+      title: 'The Silent Patient',
+      author: 'Alex Michaelides'
+    },
+    ]
--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css'
+import {data} from './books'
 // img: 'https://m.media-amazon.com/images/I/71+lh7jD7qL._AC_UL320_.jpg',
 
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/91wtqxlMHdL._AC_UL320_.jpg',
-  title: 'The Overstreet Comic Book Price',
-  author: 'Robert M. Overstreet'
-}
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/71+lh7jD7qL._AC_UL320_.jpg',
-  title: 'Cloudbuster Nine',
-  author: 'Anne R. Keene'
-}
 
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img} 
-        title={firstBook.title} 
-        author={firstBook.author}>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam ea, ipsam veniam iure odit molestias illo quasi in architecto nesciunt?</p>
-        </Book>
-      <Book
-        img={secondBook.img} 
-        title={secondBook.title} 
-        author={secondBook.author} />
+      {data.map((book) => {
+        
+        return (
+          <Book key={book.id} {...book}></Book>
+        )
+      })}
 
     </section>
   )
 }
 
-const Book = ({img, title, author, children}) => {
-  // const {img, title, author} = props
+const Book = (props) => {
+  const {img, title, author} = props
+  const clickHandler = () => {
+    alert('hello world')
+  }
+  const complexExample = (author) => {
+      console.log(author)
+  }
   return <article>
     <img src={img} alt='' />
-    <h1>{title}</h1>
+    <h1 onClick={() =>{
+      console.log(title)
+    }}>{title}</h1>
     <h4>{author}</h4>
-    {children}
+    <button type='button' onClick={clickHandler}>EXAMPLE</button>
+    <button type ='button' onClick= {() => complexExample(author)}>more complex example</button>
   </article>
 }
 
